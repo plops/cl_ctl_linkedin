1ca52f18aec7a72409794cb7e4fe2ce0bf072560
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-03-18 08:48:27 +0100

    clean up a bit

--- gen.lisp
+++ gen.lisp
@@ -27,7 +27,7 @@
 	 (imports (sys
 		   time
 		   config
-		   pathlib
+		   ;pathlib
 		   selenium
 		   selenium.webdriver
 		   selenium.webdriver.common
@@ -38,17 +38,67 @@
 		   selenium.webdriver.support.wait
 		   selenium.webdriver.support.expected_conditions
 					;selenium.webdriver.firefox
-		   pyperclip
-		   subprocess
+		   ;pyperclip
+		   ;subprocess
 		   ))
 
+
+	 (do0
+	  (def current_milli_time ()
+            (return (int (round (* 1000 (time.time))))))
+	  
+
+          (do0
+           "global g_last_timestamp"
+           (setf g_last_timestamp (current_milli_time))
+           (def milli_since_last ()
+             "global g_last_timestamp"
+             (setf current_time (current_milli_time)
+                   res (- current_time g_last_timestamp)
+                   g_last_timestamp current_time)
+             (return res)))
+
+	  (class bcolors ()
+                 (setf OKGREEN (string "\\033[92m")
+                       WARNING (string "\\033[93m")
+                       FAIL (string "\\033[91m")
+                       ENDC (string "\\033[0m")))
+	  
+          (def log (msg)
+            (print (+ bcolors.OKGREEN
+                      (dot (string "{:8d} LOG ")
+                           (format (milli_since_last)))
+                      msg
+                      bcolors.ENDC))
+            (sys.stdout.flush))
+          (def fail (msg)
+            (print (+ bcolors.FAIL
+                      (dot (string "{:8d} FAIL ")
+                           (format (milli_since_last)))
+                      msg
+                      bcolors.ENDC))
+            (sys.stdout.flush))
+          
+
+
+
+          (def warn (msg)
+            (print (+ bcolors.WARNING
+                      (dot (string "{:8d} WARNING ")
+                           (format (milli_since_last)))
+                      msg
+                      bcolors.ENDC))
+            (sys.stdout.flush)))
+	 
 	 (class SeleniumMixin (object)
 		(def __init__ (self)
+		  (log (string "SeleniumMixin::__init__"))
 		  (setf self._driver (dot selenium.webdriver
 					  #+firefox (Firefox)
 					  #+chrome (Chrome)
 				      )
-			self._wait (selenium.webdriver.support.wait.WebDriverWait self._driver 2)))
+			self._wait (selenium.webdriver.support.wait.WebDriverWait self._driver 2))
+		  (log (string "SeleniumMixin::__init__ finished")))
 	 	(def sel (self css)
 		  (log (dot (string "sel css={}")
 			    (format css)))
@@ -57,11 +107,8 @@
 		  (log (dot (string "sel xpath={}")
 			    (format xpath)))
 		  (return (self._driver.find_element_by_xpath xpath)))
-		(def wait_css_clickable (self css)
-		  (self._wait.until (selenium.webdriver.support.expected_conditions.element_to_be_clickable
-			       (tuple selenium.webdriver.common.by.By.CSS_SELECTOR
-				      css))))
 		(def wait_css_gone (self css)
+		  (log (dot (string "wait gone css={}") (format css)))
 		  (self._wait.until (selenium.webdriver.support.expected_conditions.invisibility_of_element_located
                                (tuple selenium.webdriver.common.by.By.CSS_SELECTOR
                                       css))))
@@ -71,6 +118,7 @@
                                       css))))
 
 		(def wait_xpath_clickable (self xpath)
+		  (log (dot (string "wait clickable xpath={}") (format xpath)))
 		  (self._wait.until (selenium.webdriver.support.expected_conditions.element_to_be_clickable
 			       (tuple selenium.webdriver.common.by.By.XPATH xpath))))
 		(def waitsel (self css)
@@ -79,52 +127,28 @@
 		(def waitselx (self xpath)
 		  (self.wait_xpath_clickable xpath)
 		  (return (self.selx xpath))))
-	 (def current_milli_time ()
-           (return (int (round (* 1000 (time.time))))))
+	 
 	 
 
-         (do0
-          "global g_last_timestamp"
-          (setf g_last_timestamp (current_milli_time))
-          (def milli_since_last ()
-            "global g_last_timestamp"
-            (setf current_time (current_milli_time)
-                  res (- current_time g_last_timestamp)
-                  g_last_timestamp current_time)
-            (return res)))
+	 (class LinkedIn (SeleniumMixin)
+		(def open_linkedin (self)
+		  (do0
+		   (setf site (string "https://linkedin.com"))
+		   (log (dot (string "open website {}.")
+			     (format site)))
+		   (self._driver.get site)
+		   (dot (self.sel (string "#Login")) (send_keys (aref self._config (string "linkedin_user"))))
+ 		   (dot (self.sel (string "#Password")) (send_keys (aref self._config (string "linkedin_password"))))
+		   (dot (self.selx  (string "//a[text()='Sign in']")) (click))))
+		
+		(def __init__ (self config)
+		  (SeleniumMixin.__init__ self)
+		  (setf self._config config)
+		  (self.open_linkedin)
+ 		))
 
-	 (class bcolors ()
-                (setf OKGREEN (string "\\033[92m")
-                      WARNING (string "\\033[93m")
-                      FAIL (string "\\033[91m")
-                      ENDC (string "\\033[0m")))
+	 (setf l (LinkedIn config.config))
 	 
-         (def log (msg)
-           (print (+ bcolors.OKGREEN
-                     (dot (string "{:8d} LOG ")
-                          (format (milli_since_last)))
-                     msg
-                     bcolors.ENDC))
-           (sys.stdout.flush))
-         (def fail (msg)
-           (print (+ bcolors.FAIL
-                     (dot (string "{:8d} FAIL ")
-                          (format (milli_since_last)))
-                     msg
-                     bcolors.ENDC))
-           (sys.stdout.flush))
-         
-
-
-
-         (def warn (msg)
-           (print (+ bcolors.WARNING
-                     (dot (string "{:8d} WARNING ")
-                          (format (milli_since_last)))
-                     msg
-                     bcolors.ENDC))
-           (sys.stdout.flush))
-
 	 #+nil
 	 (class
 	  LinkedIn (SeleniumMixin)
--- source/run_00_start.py
+++ source/run_00_start.py
@@ -1,7 +1,6 @@
 import sys
 import time
 import config
-import pathlib
 import selenium
 import selenium.webdriver
 import selenium.webdriver.common
@@ -11,32 +10,6 @@ import selenium.webdriver.support
 import selenium.webdriver.support.ui
 import selenium.webdriver.support.wait
 import selenium.webdriver.support.expected_conditions
-import pyperclip
-import subprocess
-class SeleniumMixin(object):
-    def __init__(self):
-        self._driver=selenium.webdriver.Firefox()
-        self._wait=selenium.webdriver.support.wait.WebDriverWait(self._driver, 2)
-    def sel(self, css):
-        log("sel css={}".format(css))
-        return self._driver.find_element_by_css_selector(css)
-    def selx(self, xpath):
-        log("sel xpath={}".format(xpath))
-        return self._driver.find_element_by_xpath(xpath)
-    def wait_css_clickable(self, css):
-        self._wait.until(selenium.webdriver.support.expected_conditions.element_to_be_clickable((selenium.webdriver.common.by.By.CSS_SELECTOR,css,)))
-    def wait_css_gone(self, css):
-        self._wait.until(selenium.webdriver.support.expected_conditions.invisibility_of_element_located((selenium.webdriver.common.by.By.CSS_SELECTOR,css,)))
-    def wait_css_clickable(self, css):
-        self._wait.until(selenium.webdriver.support.expected_conditions.element_to_be_clickable((selenium.webdriver.common.by.By.CSS_SELECTOR,css,)))
-    def wait_xpath_clickable(self, xpath):
-        self._wait.until(selenium.webdriver.support.expected_conditions.element_to_be_clickable((selenium.webdriver.common.by.By.XPATH,xpath,)))
-    def waitsel(self, css):
-        self.wait_css_clickable(css)
-        return self.sel(css)
-    def waitselx(self, xpath):
-        self.wait_xpath_clickable(xpath)
-        return self.selx(xpath)
 def current_milli_time():
     return int(round(((1000)*(time.time()))))
 global g_last_timestamp
@@ -61,3 +34,42 @@ def fail(msg):
 def warn(msg):
     print(((bcolors.WARNING)+("{:8d} WARNING ".format(milli_since_last()))+(msg)+(bcolors.ENDC)))
     sys.stdout.flush()
+class SeleniumMixin(object):
+    def __init__(self):
+        log("SeleniumMixin::__init__")
+        self._driver=selenium.webdriver.Firefox()
+        self._wait=selenium.webdriver.support.wait.WebDriverWait(self._driver, 2)
+        log("SeleniumMixin::__init__ finished")
+    def sel(self, css):
+        log("sel css={}".format(css))
+        return self._driver.find_element_by_css_selector(css)
+    def selx(self, xpath):
+        log("sel xpath={}".format(xpath))
+        return self._driver.find_element_by_xpath(xpath)
+    def wait_css_gone(self, css):
+        log("wait gone css={}".format(css))
+        self._wait.until(selenium.webdriver.support.expected_conditions.invisibility_of_element_located((selenium.webdriver.common.by.By.CSS_SELECTOR,css,)))
+    def wait_css_clickable(self, css):
+        self._wait.until(selenium.webdriver.support.expected_conditions.element_to_be_clickable((selenium.webdriver.common.by.By.CSS_SELECTOR,css,)))
+    def wait_xpath_clickable(self, xpath):
+        log("wait clickable xpath={}".format(xpath))
+        self._wait.until(selenium.webdriver.support.expected_conditions.element_to_be_clickable((selenium.webdriver.common.by.By.XPATH,xpath,)))
+    def waitsel(self, css):
+        self.wait_css_clickable(css)
+        return self.sel(css)
+    def waitselx(self, xpath):
+        self.wait_xpath_clickable(xpath)
+        return self.selx(xpath)
+class LinkedIn(SeleniumMixin):
+    def open_linkedin(self):
+        site="https://linkedin.com"
+        log("open website {}.".format(site))
+        self._driver.get(site)
+        self.sel("#Login").send_keys(self._config["linkedin_user"])
+        self.sel("#Password").send_keys(self._config["linkedin_password"])
+        self.selx("//a[text()='Sign in']").click()
+    def __init__(self, config):
+        SeleniumMixin.__init__(self)
+        self._config=config
+        self.open_linkedin()
+l=LinkedIn(config.config)
